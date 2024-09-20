e237da52c23d52bbce88afa68223b688adc2a292
sathirak <145209193+sathirak@users.noreply.github.com>
2024-09-20 13:29:00 +0530

    feat: added db config

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12732310";
-$password = "Z8ht7Jsge2"
-$database = "sql12732310";
-$port = 3306;
 
-
-$conn = new mysqli($servername, $username, $password, $database, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully";
+echo "Home Page";
