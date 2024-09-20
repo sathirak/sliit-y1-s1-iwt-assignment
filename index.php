b34c13d9fa7559e26282fb06642f970fa27a9206
sathirak <145209193+sathirak@users.noreply.github.com>
2024-09-20 12:52:36 +0530

    chore: php workflow added

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12732310";
+$password = "Z8ht7Jsge2";
+$database = "sql12732310";
+$port = 3306;
 
-echo "Hello There";
+
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
