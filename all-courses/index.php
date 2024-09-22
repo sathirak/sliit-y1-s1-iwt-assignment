7f97ff94ccdae4896c64f322b02f2db3db105715
sathirak <145209193+sathirak@users.noreply.github.com>
2024-09-22 09:12:11 +0530

    fix: issues in css and php include statements fixed

--- all-courses/index.php
+++ all-courses/index.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
+
 <head>
-    <title>Academix</title>
-
-    <?php
-    include "../components/html.php";
-    ?>
-
+    <title>AcademiX | All Courses</title>
     <link rel="stylesheet" href="styles.css">
+    <?php
+    include "../components/layout/html.php";
+    ?>
 </head>
+
 <body>
 
+
+    <?php
+    include "../components/layout/header.php";
+    ?>
+
+
     <main>
-        <h2>Browse all of our courses</h2>
+        <h1 class="heading">
+            Browse all of our courses
+        </h1>
+
         <div class="search-bar">
             <input type="text" placeholder="Search Courses...">
             <button id="searchBtn">🔍</button>
         </div>
-        
+
         <div class="courses-list">
             <div class="course-item">
                 <p>Course Title 1</p>
@@ -38,27 +47,10 @@
         </div>
     </main>
 
-    <footer>
-        <div class="contact-info">
-            <p>Contact Us</p>
-            <p>Email: support@academix.co.uk</p>
-            <p>Phone: +941234567</p>
-        </div>
-        <div class="about-info">
-            <p>About Us</p>
-            <a href="#">Careers</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Journey</a>
-            <a href="#">Terms and Conditions</a>
-        </div>
-        <div class="social-links">
-            <p>Get in touch</p>
-            <a href="#">Facebook</a>
-            <a href="#">Instagram</a>
-            <a href="#">YouTube</a>
-            <a href="#">Twitter</a>
-        </div>
-    </footer>
+    <?php
+    include "../components/layout/footer.php";
+    ?>
 
 </body>
+
 </html>
