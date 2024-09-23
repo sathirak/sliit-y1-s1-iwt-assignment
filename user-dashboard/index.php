c84ecf60da63ff9e5ae33117f35e2d56b5b842fa
Thilakshi Weliwaththa <130579853+thilu17@users.noreply.github.com>
2024-09-23 13:53:26 +0530

    feat: user dashboard

--- user-dashboard/index.php
+++ user-dashboard/index.php
@@ -36,82 +36,90 @@ session_start();
             <div class="nav">
                 <div class="search">
                     <input type="text" placeholder="Search Here">
-                    <button type="submit"><img src="search-img.jpg" alt="search icon"></button>
+                    <button type="submit"><img src="../assets/images/search-img.jpg" alt="search icon"></button>
                 </div>
                 <div class="user">
                     <a href="#" class="special-button">Profile</a>
-                    <img src="notifications.png" alt="notifications">
-                    <div class="pro-pic">
-                        <img src="user-img.png" alt="pro pic">
+                    <img src="../assets/images/notifications.png" alt="notifications">
+                    <div class="../assets/images/pro-pic">
+                        <img src="../assets/images/user-img.png" alt="pro pic">
                     </div>
                 </div>
             </div>
         </div>
         <div class="content">
-            <div class="cards">
-                <div class="card-1">
-                    <div class="box-1">
-                        <h2>My Courses</h2>
-                        <h3>Course To Do</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="course.png" alt="courses">
-                    </div>
-                </div>
 
-                <div class="card-1">
-                    <div class="box-1">
-                        <h2>Completed</h2>
-                        <h3>Learned Courses</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="completed.webp" alt="complete">
-                    </div>
-                </div>
-
-                <div class="card-1">
-                    <div class="box-1">
-                        <h2>My Status</h2>
-                        <h3>Progress</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="graph.png" alt="graph">
-                    </div>
-                </div>
-
-                <div class="card-1">
-                    <div class="box-1">
-                        <h2>Watch Time</h2>
-                        <h3>Total minutes</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="watch-time.webp" alt="watch-time">
-                    </div>
-                </div>
-            </div> <br><br>
             <div class="content-2">
                 <h1>Upcoming Courses</h1>
                 <div class="upcome">
                     <div class="new-event">
-                        <h4>Course - 01</h4>
-                    </div>
-                    <div class="new-event">
-                        <h4>Course - 02</h4>
-                    </div>
-                    <div class="new-event">
-                        <h4>Course - 03</h4>
+                        <h4>Welcome back, Thilu</h4>
                     </div>
 
+                    <div class="cards">
+                        <div class="card-1">
+                            <div class="box-1">
+                                <h2>My Courses</h2>
+                                <h3>Course To Do</h3>
+                            </div>
+                            <div class="icon-case">
+                                <img src="../assets/images/course.png" alt="courses">
+                            </div>
+                        </div>
+
+                        <div class="card-1">
+                            <div class="box-1">
+                                <h2>Completed</h2>
+                                <h3>Learned Courses</h3>
+                            </div>
+                            <div class="icon-case">
+                                <img src="../assets/images/completed.webp" alt="complete">
+                            </div>
+                        </div>
+
+                        <div class="card-1">
+                            <div class="box-1">
+                                <h2>My Status</h2>
+                                <h3>Progress</h3>
+                            </div>
+                            <div class="icon-case">
+                                <img src="../assets/images/graph.png" alt="graph">
+                            </div>
+                        </div>
+
+                        <div class="card-1">
+                            <div class="box-1">
+                                <h2>Watch Time</h2>
+                                <h3>Total minutes</h3>
+                            </div>
+                            <div class="icon-case">
+                                <img src="../assets/images/watch-time.webp" alt="watch-time">
+                            </div>
+                        </div>
+                    </div> <br><br>
+                    <div class="content-2">
+                        <h1>Upcoming Courses</h1>
+                        <div class="upcome">
+                            <div class="new-event">
+                                <h4>Course - 01</h4>
+                            </div>
+                            <div class="new-event">
+                                <h4>Course - 02</h4>
+                            </div>
+                            <div class="new-event">
+                                <h4>Course - 03</h4>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="footer">
+                    <?php
+                    include "../components/layout/footer.php";
+                    ?>
                 </div>
             </div>
-        </div>
-
-        <div class="footer">
-            <?php
-            include "../components/layout/footer.php";
-            ?>
-        </div>
-    </div>
 </body>
 
 </html>
