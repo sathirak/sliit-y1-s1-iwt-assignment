0b03bf2c033dd8d168bb9b5695365508d18ef2a7
Thilakshi Weliwaththa <130579853+thilu17@users.noreply.github.com>
2024-09-27 11:25:52 +0530

    feat: creat log in page

---
+++ log-in/log-in.php
@@ -0,0 +1,86 @@
+<?
+session_start()
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?
+    include "../components/layout/html.php";
+    ?>
+
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/components.css">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Sign In</title>
+</head>
+
+<body>
+    <div class="signup-container">
+        <div class="imag">
+            <div class="start">
+                <h1>Welcome Back!</h1>
+            </div>
+            <div class="image_1">
+                <img src="../assets/images/login-2.png" alt="login">
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="up">
+                <p>Don't have an account?
+                    <input type="button" class="button" onclick="alert('hi')" value="Sign Up">
+                </p>
+            </div>
+
+            <div class="form-content">
+                <div class="head">
+                    <div class="logo">
+                        <img src="../assets/images/logo.png" alt="logo">
+                    </div>
+                    <div class="head-2">
+                        <h2>Sign In to AcademiX</h2>
+                    </div>
+                </div>
+
+                <div class="form-signup">
+                    <form action="" class="form" method="POST" autocomplete="on">
+
+                        <div class="para">
+                            <label for="email">Email Address</label><br>
+                            <input type="email" id="email" name="email" placeholder="Enter email address" required>
+                        </div>
+
+                        <div class="pw">
+                            <label for="password">Password</label><br>
+                            <input type="password" id="password" name="password" placeholder="Enter 8 characters "
+                                required>
+                        </div>
+                        <br>
+
+                        <div class="forget">
+                            <div class="check">
+                                <label for="check">
+                                    <input type="checkbox" id="check" name="check"> Remember Me</label>
+                            </div>
+                            <div class="f-pw">
+                                <a href="#">Forgot password?</a>
+                            </div>
+                        </div>
+
+                        <br>
+                        <div class="signup">
+                            <input type="submit" class="button-signin" onclick="alert('Hii')" value="Sign Up">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+</body>
+
+</html>
