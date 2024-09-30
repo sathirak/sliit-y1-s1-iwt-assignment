31d08a5041fef7478f7e28e210703fccc605a262
Kavindu Maduhansa Liyanage <kavindumaduhansa78@gmail.com>
2024-09-30 19:48:53 +0530

    feat: create approved course dashboard

---
+++ approved-courses/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<head>
+
+    <?php
+    include "../components/layout/html.php";
+    ?>
+    <title>AcademiX | All Courses</title>
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/components.css">
+    <link rel="stylesheet" href="../styles/approved-course.css">
+
+
+</head>
+
+<body>
+
+
+    <?php
+    include "../components/layout/header.php";
+    ?>
+    
+
+<body>
+    <div class="container">
+        <nav class="sidebar">
+            <ul>
+                <li><a href="index.php">Review</a></li>
+                <li><a href="#">Watch Courses</a></li>
+                <li><a href="index.php">Approved Courses</a></li>
+            </ul>
+        </nav>
+
+        <main>
+        </h1>
+        <div class="courses-list">
+            <div class="course-box">
+            <div class="course-item">
+                <div class="course-container">
+                    <div>
+                        <p class="course-title">Physics with Samitha Rathnayake</p>
+                        <p class="course-subject">Physics</p>
+                    </div>
+
+                    <button class="view-details">View Details</button>
+                </div>
+
+                <img class="course-thumbnail" src="../assets/images/physics.jpg" alt="">
+            </div>
+            </div>
+
+            <div class="course-box">
+            <div class="course-item">
+                <div class="course-container">
+                    <div>
+                        <p class="course-title">Introductory Mathematics for A/Ls</p>
+                        <p class="course-subject">Mathematics</p>
+                    </div>
+
+                    <button class="view-details">View Details</button>               
+                </div>
+
+                <img class="course-thumbnail" src="../assets/images/maths.jpg" alt="">
+            </div>
+            </div>
+
+            <div class="course-box">
+            <div class="course-item">
+                <div class="course-container">
+                    <div>
+                        <p class="course-title">Infomation Technology for A/Ls</p>
+                        <p class="course-subject">Information Technology</p>
+                    </div>
+
+                    <button class="view-details">View Details</button>  
+                </div>
+
+                <img class="course-thumbnail" src="../assets/images/th.jpeg" alt="">
+            </div>
+            </div>
+
+        </div>
+    </main>
+
+    </div>
+
+    <?php
+    include "../components/layout/footer.php";
+    ?>
+
+</body>
+
+</html>
