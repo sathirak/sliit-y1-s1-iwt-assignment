98202350e6c427fb5b2d55ed70eab324cd1363ae
sathirak <145209193+sathirak@users.noreply.github.com>
2024-10-03 18:45:14 +0530

    feat: Improve user dashboard layout and functionality

--- components/layout/header.php
+++ components/layout/header.php
@@ -5,8 +5,7 @@
       <li><a href="/">Home</a></li>
       <li><a href="/about-us">About</a></li>
       <li><a href="/faq">FAQ</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#contact">Contact</a></li>
+      <li><a href="/contact-us">Contact</a></li>
       
     </ul>
   </nav>
--- utils/db.php
+++ utils/db.php
@@ -5,6 +5,8 @@ $password = "f2SmwnMj6g";
 $database = "sql12734758";
 $port = 3306;
 
+// https://www.phpmyadmin.co/index.php?db=sql12734758&table=course&target=sql.php
+
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
