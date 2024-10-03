b7a244e0f94e89377763d969c2ec0b4d8dca715f
sathirak <145209193+sathirak@users.noreply.github.com>
2024-10-03 17:32:14 +0530

    fix: linked header logo to home page

--- components/layout/header.php
+++ components/layout/header.php
@@ -1,16 +1,16 @@
 <header class="header">
-  <div class="logo">Academix</div>
-  <nav class="nav">
-    <ul class="nav-links">
-      <li><a href="/">Home</a></li>
-      <li><a href="/about-us">About</a></li>
-      <li><a href="/faq">FAQ</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#contact">Contact</a></li>
-      
-    </ul>
-  </nav>
+  <div class="logo"><a href="/" style="all: unset;">Academix</a></div>
+        <nav class="nav">
+          <ul class="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about-us">About</a></li>
+            <li><a href="/faq">FAQ</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
 
-  <a class="special-button" href="/sign-up/">Sign Up</a>
+          </ul>
+        </nav>
+
+        <a class="special-button" href="/sign-up/">Sign Up</a>
 
 </header>
